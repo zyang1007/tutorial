62b6a60b1d579efb82b5e6f967b875d725911c58
zyang1007 <willzhu0411@gmail.com>
2022-11-21 19:49:25 -0800

    add examples

--- src/main/java/p01_creation/MyRunnable.java
+++
@@ -1,9 +0,0 @@
-package p01_creation;
-
-public class MyRunnable implements Runnable{
-
-    @Override
-    public void run() {
-        System.out.println("Start new thread using Runnable!");
-    }
-}
--- src/main/java/p01_creation/MyThread.java
+++
@@ -1,9 +0,0 @@
-package p01_creation;
-
-public class MyThread extends Thread{
-
-    @Override
-    public void run() {
-        System.out.println("start new thread using MyThread extends Thread!");
-    }
-}
